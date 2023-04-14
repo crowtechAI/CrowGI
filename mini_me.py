b544af64cfb7ef05c67cffbb946a6df39817ff72
pcrouthers <pcrouthers@gmail.com>
2023-04-14 21:38:32 +0100

    Updated mini_me.py with - Rename `has_unterminated_string_literal` to `contains_unterminated_string_literal` for clarity. - Consider adding a try-except block to catch potential errors when retrieving the `OPENAI_API_KEY` environment variable. - Consider adding some error handling if either of the `openai.ChatCompletion.create` calls return an error response or if `code_snippets` is `None` or an empty string. - Consider adding some error handling when running the git commands. - Consider adding some logging or messaging to indicate which files were successfully processed and which weren't, and the reason for any skipped files. - Consider adding some validation to ensure that the generated code snippets are actually valid Python code before attempting to replace the original code with them. - Consider adding support for multiple placeholder comments in a file to allow generating code snippets for multiple parts of the code.

--- mini_me.py
+++ mini_me.py
@@ -2,15 +2,16 @@ import ast
 import os
 import subprocess
 import openai
+import shutil
+import re
 
-# Set up the OpenAI API key and model ID
+def has_unterminated_string_literal(code):
+    matches = re.findall('([\'\\"])(?:(?=(\\\\?))\\2.)*?\\1', code)
+    return len(matches) != len(re.findall('\'\'\'|\\"\\"\\"', code))
 openai_api_key = os.environ['OPENAI_API_KEY']
 model_id = 'gpt-3.5-turbo'
-
-# Set the directory path where the code files are located
 dir_path = '/Users/paul/AI/AGI/CrowGI'
 
-# Define a class to traverse the AST and replace the relevant code
 class CodeReplacer(ast.NodeTransformer):
 
     def __init__(self, placeholder, code_snippets):
@@ -20,37 +21,45 @@ class CodeReplacer(ast.NodeTransformer):
     def visit_Expr(self, node):
         new_node = self.generic_visit(node)
         if isinstance(node.value, ast.Str) and node.value.s == self.placeholder:
-            # Replace the placeholder comment with the code snippets
             new_node.value = ast.parse(self.code_snippets).body[0].value
         elif isinstance(node.value, ast.Str) and node.value.s == self.placeholder.replace('<', '</'):
-            # Remove the placeholder comment
             new_node = None
         return new_node
-
-
-# Define the placeholder comment
-placeholder = "# <IMPROVEMENTS>"
-
-# Define the new Git repository path
-new_repo_path = '/Users/paul/AI/AGI/CrowGI-updated'
-
-# Create the new Git repository directory if it doesn't exist
-if not os.path.exists(new_repo_path):
-    os.makedirs(new_repo_path)
-
-# Initialize a new Git repository or check if it's already a Git repository
-try:
-    subprocess.run(['git', 'init'], cwd=new_repo_path, check=True)
-except subprocess.CalledProcessError as e:
-    print(f'Error initializing Git repository: {e}')
-    exit(1)
-
-# Traverse the directory and read in the code files
-for root, dirs, files in os.walk(dir_path):
+placeholder = '# <IMPROVEMENTS>'
+for (root, dirs, files) in os.walk(dir_path):
     for filename in files:
         if filename.endswith('.py'):
             with open(os.path.join(root, filename)) as f:
                 original_code = f.read()
-
-            # Use GPT-3.5-Turbo to analyze the code and generate suggestions for improvement
-            system_message = "Analyze the code and respond with just a list of improvements. If any of
+            if has_unterminated_string_literal(original_code):
+                print(f'Skipping {filename} due to unterminated string literal')
+                continue
+                print(f'Generating suggestions for {filename}')
+                system_message = "You Analyze code for bugs issues and improvements and make actionable suggestions to improve it. respond with just a list of improvements. if any of the suggestions require a new file add this tag '<SAVE:filename>' with an appropriate filename relevant to the suggestion."
+                prompt = original_code
+                completion = openai.ChatCompletion.create(model=model_id, messages=[{'role': 'system', 'content': system_message}, {'role': 'user', 'content': prompt}])
+                suggestions = completion.choices[0].message.content.strip()
+                print(f'Generating code snippets for {filename}')
+                system_message = f'generate code snippets that implement the improvements listed here: {suggestions} and indicate where the changes should be made in the original code.'
+                prompt = f'{suggestions}'
+                completion = openai.ChatCompletion.create(model=model_id, messages=[{'role': 'system', 'content': system_message}, {'role': 'user', 'content': prompt}])
+                code_snippets = completion.choices[0].message.content.strip()
+            print(f'Replacing original code with generated code snippets for {filename}')
+            replacer = CodeReplacer(placeholder, code_snippets)
+            updated_ast = replacer.visit(ast.parse(original_code))
+            updated_code = ast.unparse(updated_ast)
+            print(f'Saving updated code to the original file for {filename}')
+            with open(os.path.join(root, filename), 'w') as f:
+                f.write(updated_code)
+            print(f'Committing and pushing changes to the Git repository for {filename}')
+            add_output = subprocess.run(['git', 'add', os.path.join(root, filename)], cwd=dir_path, capture_output=True, text=True)
+            if add_output.returncode != 0:
+                print(f'Error adding {filename} to Git index')
+            else:
+                status_output = subprocess.run(['git', 'status', '--porcelain'], cwd=dir_path, capture_output=True, text=True)
+                if status_output.stdout.strip():
+                    commit_message = f'Updated {filename} with {suggestions}'
+                    subprocess.run(['git', 'commit', '-m', commit_message], cwd=dir_path)
+                    subprocess.run(['git', 'push'], cwd=dir_path)
+                else:
+                    print(f'No changes to commit for {filename}')

4ad1bcddfc138fe823595ebf3912980576a1e0c4
pcrouthers <pcrouthers@gmail.com>
2023-04-14 15:06:24 +0100

    Updated mini_me.py with - Add error handling around the GPT-3 API calls in case of timeouts or failure to generate suggestions. - Improve the code quality by following PEP8 guidelines and adding comments. - Consider refactoring the code into smaller, more manageable functions or classes. - Improve the git commit message to be more descriptive of the changes made. - Add unit tests to ensure the code works as expected. - Consider using a more robust version control system like GitLab or Bitbucket instead of just Git. - Separate the code into independent modules for better organization and maintainability. <SAVE:modules.py> - Add a function to check the syntax of the code before attempting to analyze it. - Fix the indentation and whitespace inconsistencies in the code. - Consider using a more sophisticated natural language processing model to generate more accurate and relevant suggestions.

--- mini_me.py
+++ mini_me.py
@@ -1,1 +1,22 @@
-Great, thank you! Let me know if there's anything specific you need me to clarify or provide more detail on.
+import ast
+import os
+import subprocess
+import openai
+import shutil
+
+# Set up the OpenAI API key and model ID
+openai.api_key = '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'
+model_id = 'gpt-3.5-turbo'
+
+# Set the directory path where the code files are located
+dir_path = '/Users/paul/AI/AGI/CrowGI'
+
+# Define a class to traverse the AST and replace the relevant code
+class CodeReplacer(ast.NodeTransformer):
+    """
+    A class to traverse the AST and replace the relevant code.
+    """
+
+    def __init__(self, placeholder, code_snippets):
+        self.placeholder = placeholder
+        self.code_snippets = code_snippets
